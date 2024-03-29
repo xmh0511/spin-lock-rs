d8fb067c52e02693add1fc94d3e625d10748ed0c
xmh0511 <970252187@qq.com>
2024-03-29 14:20:26 +0800

    add thread_local for avoid deadlock

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,23 @@
+use std::cell::Cell;
 use std::cell::UnsafeCell;
 use std::ops::{Deref, DerefMut};
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread_local;
 
+thread_local! {
+    pub(crate) static CURRENT_ACQUIRED: Cell<bool> = const{ Cell::new(false) };
+}
+
+#[derive(Debug)]
 pub struct SpinLock<T> {
     state: AtomicBool,
     data: UnsafeCell<T>,
-    is_acquired: AtomicBool,
 }
 impl<T> SpinLock<T> {
     pub fn new(val: T) -> Self {
         Self {
             state: AtomicBool::new(false),
             data: UnsafeCell::new(val),
-            is_acquired: AtomicBool::new(false),
         }
     }
     pub fn lock(&self) -> SpinLockGuard<'_, T> {
@@ -23,7 +28,7 @@ impl<T> SpinLock<T> {
         {
             std::hint::spin_loop();
         }
-        self.is_acquired.store(true, Ordering::Relaxed);
+        CURRENT_ACQUIRED.set(true);
         SpinLockGuard {
             data_mut_borrow: unsafe { &mut *self.data.get() },
             state: self,
@@ -31,17 +36,17 @@ impl<T> SpinLock<T> {
     }
 
     pub fn try_lock(&self) -> std::io::Result<SpinLockGuard<'_, T>> {
-        if self.is_acquired.load(Ordering::Relaxed) {
+        if CURRENT_ACQUIRED.get() {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::WouldBlock,
-                "Re-entry the lock that has been acquired can cause a deadlock",
+                "Re-acquire the lock that has been acquired would cause a deadlock",
             ));
         }
         Ok(self.lock())
     }
 
     pub(self) fn unlock(&self) {
-        self.is_acquired.store(false, Ordering::Relaxed);
+        CURRENT_ACQUIRED.set(false);
         self.state.store(false, Ordering::Release);
     }
 }
@@ -49,23 +54,24 @@ impl<T> SpinLock<T> {
 unsafe impl<T> Send for SpinLock<T> {}
 unsafe impl<T> Sync for SpinLock<T> {}
 
-pub struct SpinLockGuard<'a, T> {
+#[derive(Debug)]
+pub struct SpinLockGuard<'a, T: 'a> {
     data_mut_borrow: &'a mut T,
     state: &'a SpinLock<T>,
 }
-impl<'a, T> Drop for SpinLockGuard<'a, T> {
+impl<'a, T: 'a> Drop for SpinLockGuard<'a, T> {
     fn drop(&mut self) {
         self.state.unlock()
     }
 }
-impl<'a, T> Deref for SpinLockGuard<'a, T> {
+impl<'a, T: 'a> Deref for SpinLockGuard<'a, T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
         self.data_mut_borrow
     }
 }
-impl<'a, T> DerefMut for SpinLockGuard<'a, T> {
+impl<'a, T: 'a> DerefMut for SpinLockGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.data_mut_borrow
     }
@@ -73,12 +79,15 @@ impl<'a, T> DerefMut for SpinLockGuard<'a, T> {
 
 #[cfg(test)]
 mod test {
+    use std::sync::Arc;
+
+    use crate::SpinLock;
+
     #[test]
     fn synchronization() {
-        use std::sync::Arc;
         let mut result = 1;
         for i in 1..=20000 {
-            let s_lock = Arc::new(crate::SpinLock::new(result));
+            let s_lock = Arc::new(SpinLock::new(result));
             let s_lock_sub = s_lock.clone();
             let spin = s_lock.clone();
             let t = std::thread::spawn(move || {
@@ -97,12 +106,11 @@ mod test {
     }
     #[test]
     fn sync_ptr() {
-        use std::sync::Arc;
         let mut result = 1;
         let b = Box::new(result);
         let mut_ptr = Box::into_raw(b);
         for i in 1..=20000 {
-            let s_lock = Arc::new(crate::SpinLock::new(mut_ptr));
+            let s_lock = Arc::new(SpinLock::new(mut_ptr));
             let s_lock_sub = s_lock.clone();
             let spin = s_lock.clone();
             let t = std::thread::spawn(move || {
@@ -125,5 +133,26 @@ mod test {
             assert_eq!(result, (i * 5) + 1);
         }
         unsafe { drop(Box::from_raw(mut_ptr)) };
+    }
+
+    #[test]
+    fn avoid_deadlock() {
+        let spin = Arc::new(SpinLock::new(0));
+        let spin2 = spin.clone();
+        let t1 = std::thread::spawn(move || {
+            let _guard = spin.lock();
+            let r = spin.try_lock();
+            println!("{:?}", r);
+            assert!(r.is_err());
+            match r {
+                Ok(_) => unreachable!(),
+                Err(e) => assert_eq!(e.kind(), std::io::ErrorKind::WouldBlock),
+            };
+        });
+        let t = std::thread::spawn(move || {
+            assert!(spin2.try_lock().is_ok());
+        });
+        t.join().unwrap();
+        t1.join().unwrap();
     }
 }
